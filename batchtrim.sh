06c2e0bc2e0d5bb670712ccce92e67b59ae49c97
Tony Kirkland <tony@tonykirkland.net>
2021-02-02 03:54:06 -0500

    fix runto to return int

--- batchtrim.sh
+++ batchtrim.sh
@@ -1,26 +1,19 @@
 #!/usr/bin/env bash
 
 START_TRIM="5"
-END_TRIM="15.0"
-
-toSeconds() {
-    awk -F: 'NF==3 { print ($1 * 3600) + ($2 * 60) + $3 }
-            NF==2 { print ($1 * 60) + $2 }
-            NF==1 { print 0 + $1 }' <<<"$1"
-}
+END_TRIM="15"
 
 getFileRuntime() {
-    ffprobe -i "$1" -show_entries format=duration -v quiet -of csv="p=0"
+    i=$(ffprobe -i "$1" -show_entries format=duration -v quiet -of csv="p=0")
+    return "${i}"
 }
 
-for FILE in /drobo/The\ Middle/*.mp4
-do
+for FILE in /drobo/mid/*.mp4; do
     [[ -e "$FILE" ]] || break
     FILE_RUNTIME=$(getFileRuntime "$FILE")
     FILE_NAME="${FILE%%.*}"
     FILE_EXT="${FILE##*.}"
     OUT_FILE="${FILE_NAME}_trimmed.${FILE_EXT}"
-
-    ffmpeg -i "$FILE" -ss "$START_TRIM" -to "$(bc <<< "$FILE_RUNTIME"-"$END_TRIM")" \
-                -c:v copy -c:a copy "$OUT_FILE"
+    echo  $FILE_RUNTIME
+    #ffmpeg -i "$FILE" -ss "$START_TRIM" -to $((FILE_RUNTIME - END_TRIM)) -c:v copy -c:a copy "$OUT_FILE"
 done
