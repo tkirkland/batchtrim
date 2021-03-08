77f0103fdec0c8db6a22187d89f0929b5717343a
Tony Kirkland <tony@tonykirkland.net>
2021-03-07 23:11:28 -0500

    fixed runtime sub

--- batchtrim.sh
+++ batchtrim.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
-START_TRIM="5"
-END_TRIM="15"
+START_TRIM="10"
+END_TRIM="25"
 
 getFileRuntime() {
-    i=$(ffprobe -i "$1" -show_entries format=duration -v quiet -of csv="p=0")
-    return "${i}"
+    ffprobe -i "$1" -show_entries format=duration -v quiet -of csv="p=0"
+    return
 }
 
-for FILE in /drobo/mid/*.mp4; do
+for FILE in /drobo/S01/*e22*.mp4; do
     [[ -e "$FILE" ]] || break
-    FILE_RUNTIME=$(getFileRuntime "$FILE")
+    FILE_RUNTIME=$(printf "%.0f" "$(getFileRuntime "$FILE")")
     FILE_NAME="${FILE%%.*}"
     FILE_EXT="${FILE##*.}"
     OUT_FILE="${FILE_NAME}_trimmed.${FILE_EXT}"
-    echo  $FILE_RUNTIME
-    #ffmpeg -i "$FILE" -ss "$START_TRIM" -to $((FILE_RUNTIME - END_TRIM)) -c:v copy -c:a copy "$OUT_FILE"
+    echo "$FILE_RUNTIME"
+    ffmpeg -i "$FILE" -ss "$START_TRIM" -to $((FILE_RUNTIME - END_TRIM)) \
+        -c:v copy -c:a copy "$OUT_FILE"
 done
